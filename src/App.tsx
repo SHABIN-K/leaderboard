405ea94d56aca57d827dbf9e1f6e23ea5ddb90a7
Shabin-k <helloshabink@gmail.com>
2024-01-14 17:28:03 +0530

    feat : convert css to tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,7 @@ const HeroSection: React.FC = () => (
     <TopCard />
     <TopCard />
     <TopCard />
+    <TopCard />
   </div>
 );
 
--- src/components/TopCard/TopCard.tsx
+++ src/components/TopCard/TopCard.tsx
@@ -1,7 +1,11 @@
 const TopCard = () => {
   return (
-    <div>TopCard</div>
-  )
-}
+    <div className="container">
+      <div className="pointContainer">
+        <h2>453</h2>
+      </div>
+    </div>
+  );
+};
 
-export default TopCard
+export default TopCard;
