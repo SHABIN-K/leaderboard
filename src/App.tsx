08ca1cb35fc62777bf06dd01474e4282fbf89295
Shabin-k <helloshabink@gmail.com>
2024-01-14 13:16:17 +0530

    feat : add animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import "./App.css";
+import { generateConfetti } from "./utils/generateConfetti";
 
 function App() {
+  useEffect(() => {
+    generateConfetti();
+  }, []);
+
   return (
     <>
       <h1>hello world</h1>
---
+++ src/utils/generateConfetti.ts
@@ -0,0 +1,31 @@
+import confetti from "canvas-confetti";
+
+const end = Date.now() + 5 * 1000;
+console.log(end);
+
+const colors = ["#ff1700", "#5800ff"];
+
+export const generateConfetti = () => {
+  (function frame() {
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 100,
+      origin: { x: 0 },
+      colors: colors,
+      shapes: ["circle", "square"],
+    });
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 100,
+      origin: { x: 1 },
+      colors: colors,
+      shapes: ["circle", "square"],
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  })();
+};
