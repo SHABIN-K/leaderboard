1f50a831170883a3a72e24fe398f07a47caa22ed
Shabin-k <helloshabink@gmail.com>
2024-01-14 18:14:48 +0530

    refactor : convert css to tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ interface SearchInputProps {
 const App: React.FC<AppProps> = () => {
   const [tableData, setTableData] = useState("");
   const [searchText, setSearchText] = useState("");
- // const [searchedData, setSearchedData] = useState<TableDataType[]>();
+  // const [searchedData, setSearchedData] = useState<TableDataType[]>();
 
   const handleSearch = () => {
     console.log("no data found");
@@ -28,8 +28,8 @@ const App: React.FC<AppProps> = () => {
 
   return (
     <>
-      <section className="appSection">
-        <div className="appContainer ">
+      <section className="relative mx-auto mt-32 mb-12">
+        <div className="px-5 lg:px-0">
           <Header />
           <HeroSection />
           <SearchInput value={searchText} onChange={handleSearch} />
@@ -43,12 +43,16 @@ const App: React.FC<AppProps> = () => {
 export default App;
 
 const Header: React.FC = () => (
-  <div className="header">
-    <h2>Pasc 2K24 Leaderboard</h2>
-    <p>Check your rank here!</p>
-    <p>
+  <div className="relative my-10 mx-auto flex justify-center items-center flex-col text-center lg:mx-8 sm:mx-2">
+    <h2 className="my-1.5 pt-0 px-2 pb-2 font-['Blanka'] text-[2rem] font-bold text-lightblack tracking-[4px] rounded-lg bg-[rgba(255,255,255,0.8)] shadow-[0_0_4px_rgba(50,69,107,0.2)] md:text-[1rem]">
+      Pasc 2K24 Leaderboard
+    </h2>
+    <p className="my-4 font-codefont text-2xl font-medium text-lightblack tracking-wide">
+      Check your rank here!
+    </p>
+    <p className="my-2 font-codefont text-lg font-medium text-lightblack tracking-wide">
       Last updated on:
-      <span>
+      <span className="mx-0.5 font-curlfont font-bold text-primarydark italic">
         {new Date(data.lastUpdated).toLocaleString("en-US", {
           dateStyle: "full",
           timeStyle: "full",
@@ -59,7 +63,7 @@ const Header: React.FC = () => (
 );
 
 const HeroSection: React.FC = () => (
-  <div className="heroSection">
+  <div className="relative my-10 px-12 flex justify-center items-center gap-8 lg:px-8 sm:px-2 md:flex-col">
     <TopCard />
     <TopCard />
     <TopCard />
@@ -68,9 +72,10 @@ const HeroSection: React.FC = () => (
 );
 
 const SearchInput: React.FC<SearchInputProps> = ({ value, onChange }) => (
-  <div className="searchInput">
+  <div className="my-12 mx-12 lg:mx-8 sm:mx-2">
     <input
       type="search"
+      className="h-14 w-full px-3 py-3 font-codefont text-lg text-lightblack font-medium shadow focus:outline-none border-[3px] border-darkgrey tracking-wider rounded-lg"
       placeholder="Search for student Name"
       value={value}
       onChange={onChange}
--- src/components/TopCard/TopCard.tsx
+++ src/components/TopCard/TopCard.tsx
@@ -1,8 +1,13 @@
 const TopCard = () => {
   return (
-    <div className="container">
-      <div className="pointContainer">
-        <h2>453</h2>
+    <div className="relative m-auto p-2 w-full flex justify-center items-center flex-col bg-darkwhite shadow-[0_0_4px_rgba(50,69,107,0.2)] rounded-[6px] z-[1] overflow-hidden">
+      <div className="m-auto w-full flex justify-center items-center flex-col bg-primarylight rounded-[6px]">
+        <h2 className="mt-2.5 mb-0 mx-auto text-4xl font-medium font-codefont text-darkwhite text-center tracking-[0.8px]">
+          453
+        </h2>
+        <p className="-mt-[2px] mb-2.5 mx-auto text-lg font-medium font-mainfont text-darkwhite text-center tracking-[0.8px]">
+          Points
+        </p>
       </div>
     </div>
   );
