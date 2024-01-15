f902b8697cf196ca6c91a18aeea6f31452d779d8
Shabin-k <helloshabink@gmail.com>
2024-01-15 20:29:28 +0530

    fix : confetti size

--- src/utils/generateConfetti.ts
+++ src/utils/generateConfetti.ts
@@ -7,21 +7,21 @@ const colors = ["#ff1700", "#5800ff"];
 export const generateConfetti = () => {
   (function frame() {
     confetti({
-      particleCount: 2,
-      angle: 60,
-      spread: 100,
-      origin: { x: 0 },
+      particleCount: 5,
+      angle: 180,
+      spread: 800,
+      origin: { y: 0 },
       colors: colors,
       shapes: ["circle", "square"],
     });
-    confetti({
-      particleCount: 2,
-      angle: 120,
-      spread: 100,
-      origin: { x: 1 },
-      colors: colors,
-      shapes: ["circle", "square"],
-    });
+  //confetti({
+  //  particleCount: 2,
+  //  angle: 120,
+  //  spread: 100,
+  //  origin: { x: 1 },
+  //  colors: colors,
+  //  shapes: ["circle", "square"],
+  //});
 
     if (Date.now() < end) {
       requestAnimationFrame(frame);
