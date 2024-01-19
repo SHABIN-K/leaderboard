578271fab10baa33291a40d8ae0c20b9dd5a4666
NISHAM-N-K <nishamnishamnk@gmail.com>
2024-01-19 15:07:48 +0530

    feat : add new logout button and add button

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -4,14 +4,48 @@ import { TableDataType } from "../types";
 import { SearchInput, Table } from "../components";
 
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { toast } from "sonner";
+import { IoMdAdd } from "react-icons/io";
 
-const Header: React.FC = () => (
-  <div className="relative my-10 mx-auto flex-center flex-col text-center lg:mx-8 sm:mx-2">
-    hell world
-  </div>
-);
+const styleDashboard = {
+  addbtn:
+    "py-3 px-4 inline-flex items-center text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none",
+};
+
+const Header: React.FC<{ navigate: NavigateFunction }> = ({ navigate }) => {
+  const handleSignOut = () => {
+    try {
+      signOut(auth).then(() => {
+        toast("You have been successfully logged out. See you next time!");
+        navigate("/");
+      });
+    } catch (error) {
+      console.error(error);
+      toast("oops! something went wrong!");
+    }
+  };
+
+  return (
+    <nav className="border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-800">
+      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+        <div className="flex items-center self-center text-xl font-semibold whitespace-nowrap text-white">
+          leaderboard
+        </div>
+        <div className="flex items-center lg:order-2">
+          <div
+            onClick={handleSignOut}
+            className="text-white focus:ring-4 bg-blue-600 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none focus:ring-gray-800 cursor-pointer"
+          >
+            Log out
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
 const DashBoard = () => {
   const navigate = useNavigate();
 
@@ -38,7 +72,17 @@ const DashBoard = () => {
 
   return (
     <div>
-      <Header />
+      <Header navigate={navigate} />
+      <div className="ml-5 mt-5 space-x-2">
+        <button className={styleDashboard.addbtn}>
+          <IoMdAdd className="w-4 h-4" />
+          Create Team
+        </button>
+        <button className={styleDashboard.addbtn}>
+          <IoMdAdd className="w-4 h-4" />
+          Add item
+        </button>
+      </div>
       <SearchInput
         value={searchText}
         setSearchText={setSearchText}
