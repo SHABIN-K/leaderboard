e8168a3c937a3b509160e70819bacf3bdab3376f
Shabin-k <helloshabink@gmail.com>
2024-01-19 21:04:36 +0530

    refactor : improve performance

--- src/utils/generateConfetti.ts
+++ src/utils/generateConfetti.ts
@@ -1,6 +1,6 @@
 import confetti from "canvas-confetti";
 
-const end = Date.now() + 2 * 1000;
+const end = Date.now() + 5 * 1000;
 
 const colors = ["#ff1700", "#5800ff"];
 
@@ -8,8 +8,8 @@ export const generateConfetti = () => {
   (function frame() {
     confetti({
       particleCount: 5,
-      angle: 180,
-      spread: 800,
+      angle: 60,
+      spread: 1000,
       origin: { y: 0 },
       colors: colors,
       shapes: ["circle", "square"],
