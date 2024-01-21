16153fc9588d563dbd4d2b0c37bf41bfe248ad4d
Shabin-k <helloshabink@gmail.com>
2024-01-21 14:59:22 +0530

    fix: My brain is fucked up, so fix it

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -6,16 +6,24 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_APIKEY,
-  projectId: import.meta.env.VITE_APIKEY,
-  storageBucket: import.meta.env.VITE_APIKEY,
-  messagingSenderId: import.meta.env.VITE_APIKEY,
-  appId: import.meta.env.VITE_APIKEY,
-  measurementId: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Initialize Cloud Firestore and authentication
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Firebase  configuration
+const fireConfig = {
+  collection: "student",
+};
+
+export { db, auth, fireConfig };
